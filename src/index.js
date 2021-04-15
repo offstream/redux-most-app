11e9d850be10f591b804085a787ab550067ae37a
offstream <evangeline.niez@gmail.com>
2021-04-16 05:33:21 +0800

    Walkback Changes To src/index.js
    Done to have history consistency

--- src/index.js
+++ src/index.js
@@ -1,29 +1,3 @@
-import { html, render } from './framework'
-import store from './store'
-import { map, skipRepeats } from './streams'
-
 import './styles.css'
-
-// App :: AppState -> TemplateResult
-const App =  state => html`
-  <pre>${JSON.stringify(state, null, 2)}</pre>
-  <button @click=${() => dispatch({ type: 'INC' })}>INC</button>
-  <button @click=${() => dispatch({ type: 'DEC' })}>DEC</button>
-`
-
-const { state$, dispatch, onChange } = store
-
-// dispatch({ type: 'INC' })
-
-// vTree$ :: Stream TemplateResult
-const vTree$ = map(App, skipRepeats(state$))
-
-// call action to have store emit the state
-// init :: () -> Void
-const initStore = () => dispatch({ type: 'INIT' })
-
-// this will also kickstart the store streams
-render(initStore, vTree$, document.getElementById('root'))
-
-onChange((curr, prev) => console.log('FIRST', `${prev} -> ${curr}`), s => s.count)
-onChange((curr, prev) => console.log('SECOND', `${prev} -> ${curr}`), s => s.count)
+const root = document.getElementById('root')
+root.innerHTML = '<h1>HELLO WORLD</h1>'
